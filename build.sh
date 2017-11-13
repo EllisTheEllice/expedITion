7d179d65f81aa2d00b95c50c14b3661fcd297f59
Simon <simon.engbers@capgemini.com>
2017-11-13 12:37:36 +0100

    .

--- build.sh
+++ build.sh
@@ -4,4 +4,11 @@ npm install
 cp -r ../contact ./src/pages/
 cp -r ../finance ./src/pages/
 cp -r ../home ./src/pages/
+apiid=$(cat /tmp/apiid)
+echo $apiid
 ionic build
+tar -czf app.tgz ./www/
+host=$(cat /tmp/dns)
+echo $host
+scp -i ~/.ssh/jenkins.pem $WORKSPACE/environment/app.tgz ubuntu@$host:/tmp/
+ssh -i ~/.ssh/jenkins.pem ubuntu@$host -c "sudo -s && cp /tmp/app.tgz /var/www && cd /var/www && tar -xzf app.tgz && mv www/* html/"
