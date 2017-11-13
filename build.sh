84c12db9bb0c84769beb214fef2caa8a32815dd2
Simon <simon.engbers@capgemini.com>
2017-11-13 14:24:46 +0100

    .

--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ tar -czf app.tgz ./www/
 host=$(cat /tmp/dns)
 echo $host
 scp -i ~/.ssh/jenkins.pem -o "StrictHostKeyChecking=no" $WORKSPACE/environment/app.tgz ubuntu@$host:/tmp/
-ssh -i ~/.ssh/jenkins.pem -o "StrictHostKeyChecking=no" ubuntu@$host -c "sudo tar -C /var/www/html -xzf /tmp/app.tgz && mv /var/www/html/www/* /var/www/html/"
+ssh -i ~/.ssh/jenkins.pem -o "StrictHostKeyChecking=no" ubuntu@$host "sudo tar -C /var/www/html -xzf /tmp/app.tgz && mv /var/www/html/www/* /var/www/html/"
