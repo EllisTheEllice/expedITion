0b0a3510de476228fb96234358c176d469d7e7bb
Simon <simon.engbers@capgemini.com>
2017-11-26 14:16:36 +0100

    .

--- contact/lambda/build.sh
+++ contact/lambda/build.sh
@@ -1,4 +1,4 @@
-export PATH=/home/tomcat/.local/bin:$PATH
+export PATH=/home/tomcat/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 cd contact/lambda
 zip index.zip index.js
 aws lambda update-function-code --function-name contactslambda --zip-file fileb://index.zip
--- finance/lambda/build.sh
+++ finance/lambda/build.sh
@@ -1,4 +1,4 @@
-export PATH=/home/tomcat/.local/bin:$PATH
+export PATH=/home/tomcat/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
 cd finance/lambda
 zip index.zip index.js
 aws lambda update-function-code --function-name transactionslambda --zip-file fileb://index.zip
